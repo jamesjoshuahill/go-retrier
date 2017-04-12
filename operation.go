0c3f780157f8d9b0c911f06b27dacbcd46ff3ec2
Josh Hill <jhill@pivotal.io>
2017-04-12 07:19:57 +0100

    Move operation interface

--- operation.go
+++
@@ -1,6 +0,0 @@
-package retry
-
-//go:generate counterfeiter -o fakes/fake_operation.go . Operation
-type Operation interface {
-	Try() (retry bool, err error)
-}
--- retrier.go
+++ retrier.go
@@ -1,5 +1,10 @@
 package retry
 
+//go:generate counterfeiter -o fakes/fake_operation.go . Operation
+type Operation interface {
+	Try() (retry bool, err error)
+}
+
 type Retrier struct {
 	operation Operation
 	tries     int
